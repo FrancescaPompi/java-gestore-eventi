08c895cbc6c360e27bf7b959b19adb24fe0aec1e
Francesca Pompi <francesca.pompi93@gmail.com>
2021-12-03 10:12:20 +0100

    costruttore e getter e setter

--- src/org/generation/italy/eventi/Evento.java
+++ src/org/generation/italy/eventi/Evento.java
@@ -1,6 +1,59 @@
 package org.generation.italy.eventi;
 
+import java.time.LocalDate;
+
 public class Evento {
+	
+	// attributi
+	private String titolo;
+	private LocalDate data;
+	private int numeroPostiTotali;
+	private int numeroPostiPrenotati;
+	
+	// costruttore
+	public Evento(String titolo, LocalDate data, int numeroPostiTotali) throws IllegalArgumentException {
+		this.titolo = titolo;
+		
+		if(data.isAfter(LocalDate.now())) {
+			this.data = data;
+		} else {
+			throw new IllegalArgumentException("Errore! La data è già passata.");
+		}
+		
+		if(numeroPostiTotali > 0) {
+			this.numeroPostiTotali = numeroPostiTotali;
+		} else {
+			throw new IllegalArgumentException("Errore! Il numero di posti totali deve essere positivo.");
+		}
+		
+		this.numeroPostiPrenotati = 0;
+	}
+	
+	
+	// getter e setter
+	public String getTitolo() {
+		return titolo;
+	}
+
+	public void setTitolo(String titolo) {
+		this.titolo = titolo;
+	}
+
+	public LocalDate getData() {
+		return data;
+	}
+
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+
+	public int getNumeroPostiTotali() {
+		return numeroPostiTotali;
+	}
+
+	public int getNumeroPostiPrenotati() {
+		return numeroPostiPrenotati;
+	}
 	
 	
 
