0978e391beef0aa25f6fe5ba5fb09e79c644a352
Francesca Pompi <francesca.pompi93@gmail.com>
2021-12-03 15:45:38 +0100

    inserita scelta utente

--- src/org/generation/italy/eventi/Main.java
+++ src/org/generation/italy/eventi/Main.java
@@ -10,10 +10,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		Scanner scan = new Scanner(System.in);
-		
-		boolean fineProgramma = false;
-		boolean erroreInput = false;
-		
+				
 		System.out.print("Inserisci il titolo dell'evento: ");
 		String titoloEvento = scan.nextLine();
 		
@@ -32,23 +29,49 @@ public class Main {
 		try {
 			Evento evento = new Evento(titoloEvento, dataEvento, postiTotaliEvento);
 			System.out.println("Hai creato l'evento: " + titoloEvento.toUpperCase() + " in data " + dataEvento + "; numero di posti: " + postiTotaliEvento);
-			System.out.print("Quanti posti vuoi prenotare? ");
-			int numeroPrenotazioni = scan.nextInt();
-			if(numeroPrenotazioni == 0) {
-				System.out.println("Non hai effettuato prenotazioni, grazie per aver usato il programma!");
-			} else {
-				for(int i = 0; i <= numeroPrenotazioni; i++) {
+			
+			String sceltaUtente;
+			boolean valid = false;
+			System.out.print("Vuoi prenotare questo evento? s/n ");
+			scan.nextLine();
+			do {
+				sceltaUtente = scan.nextLine();
+				if(sceltaUtente.equals("n")) {
+					System.out.println("Grazie per aver usato il programma!");
+					valid = true;
+					break;
+				}
+				
+				System.out.print("Quanti posti vuoi prenotare? ");
+				int numeroPrenotazioni = scan.nextInt();
+				for(int i = 0; i < numeroPrenotazioni; i++) {
 					evento.prenota();
 				}
-			}
+				System.out.println("Hai prenotato: " + numeroPrenotazioni + " posti.");
+				int postiDisponibili = postiTotaliEvento - numeroPrenotazioni;
+				System.out.println("I posti ancora disponibili sono: " + postiDisponibili);
+				
+			} while(!sceltaUtente.equals("s"));
+			
+			
+			
+//			if(numeroPrenotazioni == 0) {
+//				System.out.println("Non hai effettuato prenotazioni. Grazie per aver usato il programma!");
+//			} 
+			
+//			System.out.println("Quanti posti vuoi disdire?");
+//			int numeroDisdetti = scan.nextInt();
+//			if(numeroDisdetti == 0) {
+//				System.out.println("Nessuna disdetta: hai confermato la prenotazione effettuata.");
+//			} else if(numeroDisdetti > numeroPrenotazioni){
+//				System.out.println("Errore: il numero di posti prenotati è inferiore.");
+//			} else {
+////				for(int i = 0; )
+//			}
 		} catch (Exception e) {
 			System.out.println(e.getMessage());
 		}
-		
-		
-		
-		
-		
+
 		
 		scan.close();
 		
